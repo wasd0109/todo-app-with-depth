0b1c5cedb6f3c4cdbeaf4653aaf52d8243be9638
Cheung, Kin Ching | Ken | BSD <wasd0109.dev@gmail.com>
2025-12-26 17:51:35 +0900

    add overflow y hidden

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-y-hidden h-screen`}
       >
         <ThemeProvider>
           <Navbar />
