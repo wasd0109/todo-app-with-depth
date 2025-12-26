7ba9ab36322467ae53646f3d9e56d09b3392133f
Cheung, Kin Ching | Ken | BSD <wasd0109.dev@gmail.com>
2025-12-26 14:05:01 +0900

    base complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,54 @@
-import Image from "next/image";
+'use client';
+
+import CurvedArrow from '@/components/svg/CurvedArrow';
+import StraightArrow from '@/components/svg/StraightArrow';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const hiddenElementClass = 'opacity-0 pointer-events-none';
+  const visibleElementClass = 'opacity-100 pointer-events-auto';
+
+  const [todoInputValue, setTodoInputValue] = useState('');
+  const [showTutorial, setShowTutorial] = useState(true);
+  const [showCurvedArrow, setShowCurvedArrow] = useState(false);
+  const [showTodoInput, setShowTodoInput] = useState(false);
+  const [showSubmitButton, setShowSubmitButton] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowCurvedArrow(true);
+    }, 500)
+    setTimeout(() => {
+      setShowTodoInput(true);
+    }, 1000)
+  }, [])
+
+  const handleTodoInputChange = (todoValue: string) => {
+    setTodoInputValue(todoValue.trim());
+  }
+
+  const detectEndOfTyping = () => {
+    const doneTypingInterval = 1000; // 1 second after user stops typing
+    const typingTimer = setTimeout(() => { setShowSubmitButton(true) }, doneTypingInterval)
+    return () => clearTimeout(typingTimer)
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className='flex flex-col items-center justify-center gap-4 p-4 h-screen'>
+      <div className='relative flex items-center justify-center'>
+        <h1 className='text-2xl text-center md:text-5xl'>Add your Todo Now</h1>
+        <CurvedArrow className={`absolute left-full ml-2 md:ml-4 h-8 md:h-16 transition-opacity duration-500 ease-out  ${showCurvedArrow ? visibleElementClass : hiddenElementClass}`} scale={0.5} fill='#ffffff' />
+      </div>
+      <Input name='todo input' className={`w-4/5 md:w-1/2  transition-opacity duration-500 ease-out ${showTodoInput ? visibleElementClass : hiddenElementClass}`}
+        placeholder='By typing here'
+        value={todoInputValue}
+        onChange={(e) => handleTodoInputChange(e.target.value)}
+        onKeyUp={() => detectEndOfTyping()}
+      />
+      <StraightArrow className={`h-8 fill-white rotate-180 ${showSubmitButton ? visibleElementClass : hiddenElementClass}`} />
+      <Button className={`${showSubmitButton ? visibleElementClass : hiddenElementClass}`} variant="outline" title='Add Todo'>Add Todo</Button>
     </div>
-  );
+  )
 }
---
+++ components/svg/StraightArrow.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+const StraightArrow = (props: React.ComponentProps<"svg">) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlSpace="preserve"
+        viewBox="0 0 65.125 65.125"
+        {...props}
+    >
+        <path d="m19.032 64.337-.004-30.995H0L32.562.786l32.563 32.556H46.096v30.997H19.032v-.002zM5.641 31.007h15.723l.004 30.996 22.392-.002.005-30.994h15.721L32.563 4.089 5.641 31.007z" />
+    </svg>
+)
+export default StraightArrow
