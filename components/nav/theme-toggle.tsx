01de3354982b3474e675121c3472f20b4b43ae96
Cheung, Kin Ching | Ken | BSD <wasd0109.dev@gmail.com>
2026-01-06 15:42:10 +0900

    fix the theme toggle to reflect next state

--- components/nav/theme-toggle.tsx
+++ components/nav/theme-toggle.tsx
@@ -21,9 +21,10 @@ export default function ThemeSwitch() {
 
     return (
         <Button variant="outline" size="icon" onClick={toggleTheme}>
-            <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+            <Sun className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+            <Moon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
             <span className="sr-only">Toggle theme</span>
         </Button>
+
     )
 }
