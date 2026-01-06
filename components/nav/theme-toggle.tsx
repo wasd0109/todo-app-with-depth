48c5eaf78d26061153dcd452327a7be8c6d2c467
Cheung, Kin Ching | Ken | BSD <wasd0109.dev@gmail.com>
2026-01-06 15:38:26 +0900

    change them to toggle

--- components/nav/theme-toggle.tsx
+++ components/nav/theme-toggle.tsx
@@ -15,26 +15,15 @@ import {
 export default function ThemeSwitch() {
     const { setTheme } = useTheme()
 
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"))
+    }
+
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon">
-                    <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-                    <span className="sr-only">Toggle theme</span>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="start">
-                <DropdownMenuItem onClick={() => setTheme("light")}>
-                    Light
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("dark")}>
-                    Dark
-                </DropdownMenuItem>
-                {/* <DropdownMenuItem onClick={() => setTheme("system")}>
-                    System
-                </DropdownMenuItem> */}
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <Button variant="outline" size="icon" onClick={toggleTheme}>
+            <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+            <span className="sr-only">Toggle theme</span>
+        </Button>
     )
 }
